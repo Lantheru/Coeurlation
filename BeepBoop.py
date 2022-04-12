79048a31e59058658478bc1bb1e79ed65ff0ff56
J. Torres <Joseph.a.torres@gmail.com>
2022-04-12 02:40:27 -0700

    Worked out assembling the pipelines from Universalis to discord bot. Implemented bot method to interpret text into the urls to send.. Item names are now pulled from database to accompany item ids.

--- BeepBoop.py
+++ BeepBoop.py
@@ -1,4 +1,5 @@
 import os
+import io
 import random
 import discord
 import MarketApp
@@ -9,10 +10,9 @@ import dataframe_image as dfi
 import re
 
 servers = ['Balmung', 'Brynhildr', 'Coeurl', 'Diabolos', 'Goblin', 'Malboro', 'Mateus', 'Zalera']
+bot = CoeurlConnection(user='bbbot')
 
-def _makefile(df):
-    dfi.export(df, 'df_img.png')
-
+#need to update: records message that prompted a command and saves it into bbtestlog.txt
 def _logmessage(message, whattowrite = None):
     message = message
     if whattowrite == None:
@@ -22,6 +22,16 @@ def _logmessage(message, whattowrite = None):
         log.write(whattowrite)
 
         
+#saves dataframe as image so it can be sent through discord and isn't character limited
+# def _makefile(df, name = 'uniquery'):
+#     with open('uniquery.png','w') as myfile:
+#         dfi.export(df, myfile, max_rows= -1)
+#         to_disc = myfile
+#         myfile.close()
+#         return to_disc
+    # dfi.export(df, name, max_rows= -1)
+
+        
 
 
 load_dotenv()
@@ -29,9 +39,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-
+bot = CoeurlConnection()
 
 @client.event
+##ready message when logged in
 async def on_ready():
     for guild in client.guilds:
         print(guild)
@@ -43,43 +54,57 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
+## when you read a message, do:
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if 'help' in message.content:
-        await message.channel.send('My current commands are: help, test')
-        return
+    words = message.content.lower().split()
 
-    if 'bb!' in message.content:
+    if 'bb!' in words:
         _logmessage(message)
-
         
-        if 'test' in message.content:
-            if 'itemlookup' in message.content:
-                user_string = re.search(r'"(.*?)"',message.content)
-                user_string = user_string.group()
-                print(user_string)
-                data = bot.itemlookup(user_string)
-                if len(data) == 0:
-                    await message.channel.send('I did not find the thing, sheeeiiiit.')
-                    
+        
+        if 'help' in words:
+            await message.channel.send(
+                'Hello! Please make sure all commands are preceeded by "bb!". My commands are currently:\n\nLookupitem: Takes a word enveloped by double quotation marks and forms a list of matching names and their FFXIV id numbers.\n**Example**\nbb! lookupitem "materia"\n\nCurrentlisting or Salehistory: Returns instances of the item currently listed in the marketplace or its history of sales in the last week. Please provide server names within @ symbols and separated by commas and item ids within $ symbols separated by commas.\n **Example**\nbb! currentlistings @Coeurl,Balmung@ #6548,7490#'
+                                        )
+            return
 
-                if len(data) > 50:
-                    await message.channel.send('bruh be more specific. That produced {} results'.format(len(data)))
-                    
-                if len(data) <= 50 and len(data) > 0:
-                    _makefile(data)
-                    await message.channel.send(file = discord.File('df_img.png'))
-            # for type in ['salehistory', 'currentlistings']:
-            #     if type in message.content:
-            #         query = Uniquery(querytype = type)
-            
-            
-            
-            # _makefile(worldlist)
+        if 'lookupitem' in words:
+            user_string = re.search(r'"(.*?)"',message.content)
+            user_string = user_string.group().strip('"')
+            data = bot.itemlookup(user_string)
+            if len(data) == 0:
+                await message.channel.send('I did not find the thing, sadface dot execute')
+                
 
+            if len(data) > 50:
+                await message.channel.send('That produced {} results. I\'m limited to 50'.format(len(data)))
+                
+            if len(data) <= 50 and len(data) > 0:
+                _makefile(data, 'lookup.png')
+                await message.channel.send(file = discord.File('lookup.png'))
+        
+        elif 'currentlistings' or 'salehistory' in words:
+            if 'salehistory' in words:
+                user_querytype = 'salehistory'
+            else:
+                user_querytype = 'currentlistings'
+            user_world = re.search(r'@(.*?)@',message.content)
+            user_world = user_world.group().replace("@","")
+           
+            user_itemids = re.search(r'\$(.*?)\$',message.content)
+           
+            user_itemids = user_itemids.group().replace("$","")
+           
+            user_query = UniQuery(user_world, user_itemids, user_querytype)
+            df = user_query.df
+            # data = bot.itemlookup(user_string)
             
+            botresponse=df.to_csv()
+            await message.channel.send(file = "{}&{}.xlsx".format(user_world,user_itemids))
+
 
 client.run(TOKEN)
